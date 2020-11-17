199bbc3160e5124cecdea3278bfd3457f3717b47
shivamrathi99 <rathishivam99@gmail.com>
2020-11-17 14:18:49 +0530

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+import greet from "./components/greet.js"
+function App() {
+  return (
+    <div className="App">
+     <greet>  </greet>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/greet.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const greet= () => <h1> hello shivam!!! </h1>;
+
+export default greet
