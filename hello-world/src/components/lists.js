27a8c477047a1b3f0089d2e5d7ec823ebe8c71d6
shivamrathi99 <rathishivam99@gmail.com>
2020-11-21 16:31:55 +0530

    Form Handling

--- hello-world/src/components/lists.js
+++ hello-world/src/components/lists.js
@@ -3,12 +3,23 @@ import './style.css'
 
 
 class Lists extends Component {
+
+    submit = () => {
+        alert("DONE")
+    }
     render() {
-        const arr = ["shubham", "shivam", "abc", "ashsdhaskjdd", "qwe", "123" ,"$%%$", "dsfds", "121212"]
+        // const arr = ["shubham", "shivam", "abc", "ashsdhaskjdd", "qwe", "123" ,"$%%$", "dsfds", "121212"]
         return (
-            <div className = "divstyle">
-                {arr.map((item) => <div key={item}>{item}</div>  )}
-            </div>
+            // <div className = "divstyle">
+            //     {arr.map((item) => <div key={item}>{item}</div>  )}
+            // </div>
+
+            <form onSubmit={this.submit}>
+                <input type="text" />
+                <input type="number" />
+                <input type="submit" />
+            </form>
+
         )
     }
 }
